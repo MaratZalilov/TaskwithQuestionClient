dd7dc9c7575f1b202b00a0b97636ab151b1f7b32
Zalilov Marat <Zalilov Marat@DESKTOP-BMGRAJ6>
2024-03-15 08:08:05 +0300

    V0.0.0.1

--- TaskwithQuestionClient/ClientLogicProgram.cs
+++ TaskwithQuestionClient/ClientLogicProgram.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Sockets;
@@ -40,5 +41,32 @@ namespace TaskwithQuestionClient
                 MessageBox.Show(Encoding.UTF8.GetString(buffer));
             }
         }
+
+        public void CreateButton()
+        {
+
+            int W = 350, H = 250;
+            for (int j = 0; j < 3; j++)
+            {
+                Button button = new Button();
+                button.BackColor = Color.Orange;
+                button.Name = $"Buttons" + $"{j}";
+                button.Text = $"{j}";
+                button.Size = new Size(200, 50);
+                button.Location = new Point(W, H += 50);
+                button.Click += Buttons_Click;
+                MainForm.ActiveForm.Controls.Add(button);
+
+            }
+        }
+
+        private void Buttons_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            MainForm.ActiveForm.Controls.Clear();
+            CreateButton();
+            MessageBox.Show(b.Name);
+        }
+
     }
 }
